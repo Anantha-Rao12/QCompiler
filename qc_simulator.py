f47483685f859669d050715997d35aef6936bc90
Anantha-Rao12 <aanantha.s.rao@gmail.com>
2021-02-01 18:40:42 +0530

    added typing checking

--- qc_simulator.py
+++ qc_simulator.py
@@ -1,34 +1,42 @@
 import numpy as np
 from collections import Counter
 from itertools import product
+from typing import Optional,Dict
 
 
 class QuantumCircuit():
     
+    """Implement a simple Quantum Circuit Compiler"""
+    
     ### Class Variables
     
     # Computational Basis
-    Zero = np.array([[1.0],[0.0]])
-    One = np.array([[0.0],[1.0]])
+    Zero : np.ndarray = np.array([[1.0],[0.0]])
+    One : np.ndarray = np.array([[0.0],[1.0]])
     
     # Projection operators
-    P0 = Zero @  Zero.T
-    P1 = One @ One.T
+    P0 : np.ndarray = Zero @  Zero.T
+    P1 : np.ndarray = One @ One.T
 
     # Single Qubit gates
-    X = np.array([[0,1],[1,0]],dtype = 'complex_')
-    Y = np.array([[0,0-1.j],[0+1.j,0]],dtype = 'complex_')
-    Z = np.array([[1,0],[0,1]],dtype = 'complex_')
-    H = (1/np.sqrt(2))*np.array([[1,1],[1,-1]],dtype = 'complex_')
-    I2 = np.array([[1,0],[0,1]],dtype = 'complex_')
+    X : np.ndarray = np.array([[0,1],[1,0]],dtype = 'complex_')
+    Y : np.ndarray = np.array([[0,0-1.j],[0+1.j,0]],dtype = 'complex_')
+    Z : np.ndarray = np.array([[1,0],[0,1]],dtype = 'complex_')
+    H : np.ndarray = (1/np.sqrt(2))*np.array([[1,1],[1,-1]],dtype = 'complex_')
+    I2 : np.ndarray = np.array([[1,0],[0,1]],dtype = 'complex_')
 
-    def __init__(self,nqubits:int):
+    def __init__(self,nqubits:int) -> None:
+        if nqubits <= 0 : 
+            raise ValueError(f'Number of qubits must be positive. Given {nqubits}')
+            
         self.nqubits = nqubits
            
-    def get_groundstate(self):
+    def get_groundstate(self)-> np.ndarray :
+        """Tensor product of #nqubit systems in Zero state"""
+        
         return self.ntensor_product(*[QuantumCircuit.Zero]*self.nqubits)
     
-    def get_std_basis(self):
+    def get_std_basis(self)-> np.ndarray :
         """Returns a list of standard basis states for a composite system of #nqubits"""
 
         # obtain a list of all combinations of (0,1) foe given no.of qubits
@@ -38,34 +46,43 @@ class QuantumCircuit():
         return list(map(lambda x : '|'+str(x)+'>',basis_states)) # modify into ket vectors
 
     
-    def get_finalstate(self,program,istate):
-        """Given a program (quantum-circuit), execute it on a given initial state"""
-
-        fstate = np.copy(istate) # copy the istate vector for fstate
+    def get_finalstate(self,
+                       program:Dict,
+                       initial_state:Optional[np.ndarray] = None)-> np.ndarray:
+        """Given a program (quantum-circuit), execute it on a given initial state and return final state"""
+        
+        if initial_state is None:
+            initial_state = self.get_groundstate()
+        
+        fstate = np.copy(initial_state) # copy the initialstate vector for finalstate
+        
         for operation in program:
-            #print(self.nqubits)
-            #print(type(operation['gate']),operation['target'])
             matrix = self.get_operator(operation['gate'],operation['target'])
             fstate = matrix @ fstate  # matrix product
         return fstate
 
-    def get_operator(self,gatename,target_qubits):
+    
+    def get_operator(self,gatename:str,target_qubits:list) -> np.ndarray:
         """Return the matrix for a specified Q-gate, no of qubits and target_qubit"""
 
+        
         single_qgates = {'I2':QuantumCircuit.I2,'X':QuantumCircuit.X,
                          'Y':QuantumCircuit.Y,'Z':QuantumCircuit.Z,
                          'H':QuantumCircuit.H}
-        
-        
-        if gatename in single_qgates:
 
-            # create an array of Identity operations of len = no of qubits
+        if target_qubits[0] < 0 or target_qubits[0] > self.nqubits:
+            raise IndexError(f'Incorrect entry for target qubit. Please try a value between 0 and {self.nqubits}')
             
+        #if gatename not in single_qgates or gatename != 'CNOT':
+        #    raise ValueError(f'Incorrect gate name. Expecting only {single_qgates.keys()} gates or CNOT')
+
+        elif gatename in single_qgates:
+
+            # create an array of Identity operations of len = no of qubits 
             multiqubitgate_list = np.array([single_qgates['I2'] for i in range(self.nqubits)])
             # replace Id by Unitary Gate for the target qubit
             multiqubitgate_list[target_qubits[0]] = single_qgates[gatename]
             result = QuantumCircuit.ntensor_product(*multiqubitgate_list)
-
 
         elif gatename == 'CNOT': 
             # for CNOT we need two inputs the (control,target) qubits
@@ -82,16 +99,21 @@ class QuantumCircuit():
             multiqubitgate_list2[target] = single_qgates['X']
 
             result = QuantumCircuit.ntensor_product(*multiqubitgate_list1) + QuantumCircuit.ntensor_product(*multiqubitgate_list2) 
-            
+                
         return result
+            
+            
+            
+ 
         
-    def measurement(self,statevector):
+    def measurement(self,statevector:np.ndarray) -> int :
         """Perform measurement on given statevector"""
+        
         probability_dist = abs((statevector**2).real)
         return np.random.choice(range(len(statevector)),1, p=probability_dist.flatten())[0]
     
 
-    def get_counts(self,statevector,shots):
+    def get_counts(self,statevector:np.ndarray,shots:int) -> Dict[str, int]:
         """Get Counts for given number of measurement shots"""
 
         measurement_results = [self.measurement(statevector) for i in range(shots)] #get measurements for all shots
@@ -105,10 +127,16 @@ class QuantumCircuit():
         return dict(zip(newkeys_for_counter,values))
     
     @classmethod
-    def ntensor_product(cls,*args):
+    def ntensor_product(cls,*args) -> np.ndarray:
         """Calculate the Tensor product over a variable number of inputs"""
         result = np.array([[1.0]])
         for element in args:
             result = np.kron(result, element)
         return result
+    
+    @staticmethod
+    def normalize_state(vector:np.ndarray) -> np.ndarray:
+        """Returns a normalized vector"""
+        
+        return vectore/np.sum(vector**2)
 
